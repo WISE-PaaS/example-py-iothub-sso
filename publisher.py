2aeae7becf8fa99c32be8c51c03d0271fdc60d67
tsian077 <tsian077@gmail.com>
2019-07-20 15:14:30 +0800

    Update publisher.py

--- publisher.py
+++ publisher.py
@@ -1,11 +1,11 @@
 import paho.mqtt.client as mqtt
 #externalHosts
-broker="40.81.27.10"
+broker="xx.81.xx.10"
 #mqtt_port
 mqtt_port=1883
 #mqtt_username
-username="f456d95d-b76f-43e9-8b35-bac8383bf941:5366fbed-f8e4-4c8f-99ce-8faf6575dfe4"
-password="y4aVpae5yi4Edjr00AQ6oG9NB"
+username="xxxxxxxx-b76f-43e9-8b35-bac8383bf941:5366fbed-f8e4-4c8f-99ce-8faf6575dfe4"
+password="xxxxxxxxi4Edjr00AQ6oG9NB"
 def on_publish(client,userdata,result):             #create function for callback
     print("data published")
    
